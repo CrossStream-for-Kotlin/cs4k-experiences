edc20291c8c1ab69ec5a56fded8384eaccc5dc8b
joaobonachoA49437 <A49437@alunos.isel.pt>
2024-03-24 15:46:30 +0000

    improves AssociatedSubscribers.kt

--- code/prototype/app/src/main/kotlin/cs4k/prototype/broker/AssociatedSubscribers.kt
+++ code/prototype/app/src/main/kotlin/cs4k/prototype/broker/AssociatedSubscribers.kt
@@ -14,11 +14,12 @@ class AssociatedSubscribers {
 
     fun addToKey(topic: String, subscriber: Subscriber) = lock.withLock {
         val currentSubscribers = map[topic] ?: emptyList()
-        map[topic] = if (currentSubscribers.isEmpty()) listOf(subscriber) else (currentSubscribers as MutableList) + subscriber
+        map[topic] = currentSubscribers + subscriber
     }
 
     fun removeIf(topic: String, predicate: (Subscriber) -> Boolean) = lock.withLock {
-        val toRemove = (map[topic]?.filter { predicate(it) } ?: return@withLock)
-        map.remove(topic, toRemove)
+        val currentSubscribers = map[topic]
+        val subscriberToRemove = (currentSubscribers?.find { predicate(it) } ?: return@withLock)
+        map[topic] = currentSubscribers - subscriberToRemove
     }
 }
