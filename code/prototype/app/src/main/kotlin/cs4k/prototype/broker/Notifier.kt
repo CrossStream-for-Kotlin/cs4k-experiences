0d46a9616393aede1566a4aac6d2f6063cec2070
joaobonachoA49437 <A49437@alunos.isel.pt>
2024-03-24 15:35:49 +0000

    improves Notifier.kt

--- code/prototype/app/src/main/kotlin/cs4k/prototype/broker/Notifier.kt
+++ code/prototype/app/src/main/kotlin/cs4k/prototype/broker/Notifier.kt
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.sql.Connection
 import java.sql.DriverManager
-import java.util.*
+import java.util.UUID
 import kotlin.concurrent.thread
 
 @Component
@@ -13,33 +13,36 @@ class Notifier {
 
     private val channel = "share_channel"
     private val connection = createConnection()
-    private val subscriberQueue = AssociatedSubscribers()
-    // ConcurrentLinkedQueue<Subscriber>()
+    private val associatedSubscribers = AssociatedSubscribers()
 
     init {
-        createNotifierTable()
+        createEventsTable()
 
         thread {
-            logger.info("listen channel '{}'", channel)
-            connection.createStatement().use { it.execute("LISTEN $channel;") }
+            listen()
             waitForNotification()
         }
     }
 
     fun subscribe(topic: String, handler: (event: Event) -> Unit): () -> Unit {
-        logger.info("new subscriber topic '{}'", topic)
         val subscriberId = UUID.randomUUID()
-        subscriberQueue.addToKey(topic, Subscriber(subscriberId, handler))
-        // subscriberQueue.add(Subscriber(topic, handler))
+        associatedSubscribers.addToKey(topic, Subscriber(subscriberId, handler))
+        logger.info("new subscriber topic '{}' id '{}", topic, subscriberId)
+
         getLastEvent(topic)?.let { event -> handler(event) }
-        return { unListen(topic, subscriberId) }
+
+        return { unsubscribe(topic, subscriberId) }
     }
 
-    fun publish(topic: String, message: String, isLastMessage: Boolean = false) {
-        notify(topic, message, isLastMessage)
+    fun publish(topic: String, message: String, isLast: Boolean = false) {
+        notify(topic, message, isLast)
     }
 
-    // TODO("Remove from subscriberQueue")
+    private fun unsubscribe(topic: String, subscriberId: UUID) {
+        associatedSubscribers.removeIf(topic) { subscriber -> subscriber.id == subscriberId }
+        logger.info("unsubscribe topic '{}' id '{}", topic, subscriberId)
+    }
+
     private fun waitForNotification() {
         val pgConnection = connection.unwrap(PGConnection::class.java)
 
@@ -49,102 +52,110 @@ class Notifier {
                 val payload = notification.parameter
                 logger.info("new notification [{}]", payload)
                 val event = createEvent(payload)
-                subscriberQueue
+                associatedSubscribers
                     .getAll(event.topic)
-                    // .filter { subscriber -> subscriber.topic == event.topic }
                     .forEach { subscriber -> subscriber.handler(event) }
             }
         }
     }
 
     private fun createEvent(payload: String): Event {
-        // TOPIC||ID||MESSAGE||[done]
+        // TOPIC||ID||MESSAGE||[isLast]
         val splitPayload = payload.split("||")
         return Event(
             topic = splitPayload[0],
             id = splitPayload[1].toLong(),
             message = splitPayload[2],
-            isLast = splitPayload.size > 3 && splitPayload[3] == ("done")
+            isLast = splitPayload.size > 3 && splitPayload[3] == ("isLast")
         )
     }
 
-    private fun notify(topic: String, message: String, isLastMessage: Boolean) {
-        val connection = createConnection()
-
-        val id = getEventIdAndUpdateHistory(connection, topic, message)
-        val payload = if (isLastMessage) "$topic||$id||$message||done" else "$topic||$id||$message"
-        logger.info("notify topic '{}' [{}]", topic, payload)
-
-        connection.use {
-            // 'select pg_notify()' is used because NOTIFY cannot be used in preparedStatement.
-            // query results are ignored, but notifications are still sent.
-            val stm = it.prepareStatement("select pg_notify(?, ?)")
-            stm.setString(1, channel)
-            stm.setString(2, payload)
-            stm.execute()
+    private fun listen() {
+        connection.createStatement().use { stm ->
+            stm.execute("listen $channel;")
         }
+        logger.info("listen channel '{}'", channel)
     }
 
-    // TODO("Call unListen")
-    private fun unListen(topic: String, subscriberId: UUID) {
+    private fun unListen() {
+        connection.createStatement().use { stm ->
+            stm.execute("unListen $channel;")
+        }
         logger.info("unListen channel '{}'", channel)
-        subscriberQueue.removeIf(topic) { subs -> subs.id == subscriberId }
+    }
+
+    private fun notify(topic: String, message: String, isLast: Boolean) {
+        createConnection().use { conn ->
+            conn.autoCommit = false
+            conn.transactionIsolation = Connection.TRANSACTION_SERIALIZABLE
+
+            val id = getEventIdAndUpdateHistory(conn, topic, message, isLast)
+            val payload = if (isLast) "$topic||$id||$message||isLast" else "$topic||$id||$message"
+
+            // 'select pg_notify()' is used because NOTIFY cannot be used in preparedStatement.
+            // query results are ignored, but notifications are still sent.
+            conn.prepareStatement("select pg_notify(?, ?)").use { stm ->
+                stm.setString(1, channel)
+                stm.setString(2, payload)
+                stm.execute()
+            }
+
+            conn.commit()
+            conn.autoCommit = true
+
+            logger.info("notify topic '{}' [{}]", topic, payload)
+        }
     }
 
     private fun getLastEvent(topic: String): Event? {
-        createConnection().prepareStatement("select id, message from notifier where topic = ? for share;").use { stm ->
-            stm.setString(1, topic)
-            val rs = stm.executeQuery()
-            return if (rs.next()) {
-                Event(
-                    topic = topic,
-                    id = rs.getLong("id"),
-                    message = rs.getString("message")
-                )
-            } else {
-                null
+        createConnection().use { conn ->
+            conn.prepareStatement("select id, message, is_last from events where topic = ? for share;").use { stm ->
+                stm.setString(1, topic)
+                val rs = stm.executeQuery()
+                return if (rs.next()) {
+                    Event(
+                        topic = topic,
+                        id = rs.getLong("id"),
+                        message = rs.getString("message"),
+                        isLast = rs.getBoolean("is_last")
+                    )
+                } else {
+                    null
+                }
             }
         }
     }
 
-    // TODO("Transaction level")
-    private fun getEventIdAndUpdateHistory(connection: Connection, topic: String, message: String): Long {
-        connection.prepareStatement("select id from notifier where topic = ?;").use { stm ->
+    private fun getEventIdAndUpdateHistory(conn: Connection, topic: String, message: String, isLast: Boolean): Long {
+        conn.prepareStatement(
+            """
+                insert into events (topic, message) 
+                values (?, ?) 
+                on conflict (topic) do update 
+                set id = events.id + 1, message = excluded.message, is_last = ? 
+                returning id;
+            """.trimIndent()
+        ).use { stm ->
             stm.setString(1, topic)
+            stm.setString(2, message)
+            stm.setBoolean(3, isLast)
             val rs = stm.executeQuery()
-            if (rs.next()) {
-                val newEventId = rs.getLong("id") + 1
-                updateLastEvent(connection, newEventId, message, topic)
-                return newEventId
-            } else {
-                insertFirstEventOfTopic(connection, message, topic)
-                return 0
-            }
+            return if (rs.next()) rs.getLong("id") else error("ResultSet must have next.")
         }
     }
 
-    // TODO("Transaction level")
-    private fun insertFirstEventOfTopic(connection: Connection, message: String, topic: String) {
-        connection.prepareStatement("insert into notifier (topic, id, message) values (?, 0, ?);").use { stm ->
-            stm.setString(1, topic)
-            stm.setString(2, message)
-            stm.executeUpdate()
-        }
-    }
-
-    // TODO("Transaction level")
-    private fun updateLastEvent(connection: Connection, id: Long, message: String, topic: String) {
-        connection.prepareStatement("update notifier set id = ? , message = ? where topic = ?;").use { stm ->
-            stm.setLong(1, id)
-            stm.setString(2, message)
-            stm.setString(3, topic)
-            stm.executeUpdate()
-        }
-    }
-
-    private fun createNotifierTable() {
+    private fun createEventsTable() {
         connection.createStatement().use { stm ->
-            stm.execute("create table if not exists notifier (topic varchar(255), id integer, message varchar(255));")
+            stm.execute(
+                """
+                    create table if not exists events (
+                        topic varchar(128) primary key, 
+                        id integer default 0, 
+                        message varchar(512), 
+                        is_last boolean default false
+                    );
+                """.trimIndent()
+            )
         }
     }
 
@@ -158,3 +169,11 @@ class Notifier {
         }
     }
 }
+
+/**
+ * TODO
+ * - Call unListen at the end;
+ * - Error handling;
+ * - Change the call to createConnection for each 'getLastEvent' and 'notify' for an elastic connection poll;
+ * - Try reduce 'notify' isolation level.
+ */
