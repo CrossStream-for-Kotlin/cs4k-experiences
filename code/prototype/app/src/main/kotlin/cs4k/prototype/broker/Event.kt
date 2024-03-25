348f5dc27fe98520f8b09aa76ff16deeeee2e684
Tiago <a48666@alunos.isel.pt>
2024-03-25 20:29:20 +0000

    comments

--- code/prototype/app/src/main/kotlin/cs4k/prototype/broker/Event.kt
+++ code/prototype/app/src/main/kotlin/cs4k/prototype/broker/Event.kt
@@ -1,5 +1,12 @@
 package cs4k.prototype.broker
 
+/**
+ * Represents an event that can be published to a topic.
+ * @param topic the topic of the event.
+ * @param id the id of the event.
+ * @param message the message of the event.
+ *  @param isLast if the event is the last one.
+ */
 data class Event(
     val topic: String,
     val id: Long,
--- code/prototype/app/src/main/kotlin/cs4k/prototype/broker/Subscriber.kt
+++ code/prototype/app/src/main/kotlin/cs4k/prototype/broker/Subscriber.kt
@@ -2,6 +2,11 @@ package cs4k.prototype.broker
 
 import java.util.UUID
 
+/**
+ * Represents a subscriber to a topic.
+ * @param id the id of the subscriber.
+ * @param handler the handler to call when an event is published to the topic.
+ */
 class Subscriber(
     val id: UUID,
     val handler: (event: Event) -> Unit
