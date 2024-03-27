0fa275d057ef4fd75b557ea001f441b798f88749
joaobonachoA49437 <A49437@alunos.isel.pt>
2024-03-27 16:29:47 +0000

    improves AssociatedSubscribers, using compute and computeIfPresent functions

--- code/prototype/app/src/main/kotlin/cs4k/prototype/broker/AssociatedSubscribers.kt
+++ code/prototype/app/src/main/kotlin/cs4k/prototype/broker/AssociatedSubscribers.kt
@@ -4,43 +4,50 @@ import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
 /**
- * This class is responsible for managing the association between topics and subscribers.
+ * This class is responsible for managing the association between topics and [Subscriber]s.
  * It is thread-safe.
  */
 class AssociatedSubscribers {
 
-    // Map that associates a topic with a list of subscribers.
-    private val map = mutableMapOf<String, List<Subscriber>>()
+    // Map that associates topics with lists of subscribers.
+    private val map = HashMap<String, List<Subscriber>>()
 
     // Lock to ensure thread safety.
     private val lock = ReentrantLock()
 
     /**
-     * Returns all subscribers associated with a topic.
+     * Get all subscribers associated with a topic.
      * @param topic the topic to get the subscribers from.
+     * @return the list of subscribers associated with the topic.
      */
     fun getAll(topic: String) = lock.withLock {
         map[topic] ?: emptyList()
     }
 
     /**
-     * Adds a subscriber to a topic.
+     * Add a subscriber to a topic.
      * @param topic the topic to add the subscriber to.
      * @param subscriber the subscriber to add.
      */
-    fun addToKey(topic: String, subscriber: Subscriber) = lock.withLock {
-        val currentSubscribers = map[topic] ?: emptyList()
-        map[topic] = currentSubscribers + subscriber
+    fun addToKey(topic: String, subscriber: Subscriber) {
+        lock.withLock {
+            map.compute(topic) { _, subscribers ->
+                subscribers?.let { it + subscriber } ?: listOf(subscriber)
+            }
+        }
     }
 
     /**
-     * Removes a subscriber from a topic.
+     * Remove a subscriber from a topic.
      * @param topic the topic to remove the subscriber from.
      * @param predicate a predicate to determine which subscriber to remove.
      */
-    fun removeIf(topic: String, predicate: (Subscriber) -> Boolean) = lock.withLock {
-        val currentSubscribers = map[topic]
-        val subscriberToRemove = (currentSubscribers?.find { predicate(it) } ?: return@withLock)
-        map[topic] = currentSubscribers - subscriberToRemove
+    fun removeIf(topic: String, predicate: (Subscriber) -> Boolean) {
+        lock.withLock {
+            map.computeIfPresent(topic) { _, subscribers ->
+                val subscriberToRemove = subscribers.find(predicate) ?: return@computeIfPresent subscribers
+                (subscribers - subscriberToRemove).ifEmpty { null }
+            }
+        }
     }
 }
